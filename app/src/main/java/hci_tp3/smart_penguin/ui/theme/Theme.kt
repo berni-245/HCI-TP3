b7b63a462c8e23dc0aae689533df2a2e2e2774bf
berni <bernizapico@gmail.com>
2024-06-13 15:57:46 -0300

    Chore: removed some settings that we will not use

--- app/src/main/java/hci_tp3/smart_penguin/ui/theme/Theme.kt
+++ app/src/main/java/hci_tp3/smart_penguin/ui/theme/Theme.kt
@@ -1,8 +1,6 @@
 package hci_tp3.smart_penguin.ui.theme
 
-import android.app.Activity
 import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
@@ -35,9 +33,9 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun HCITP3Theme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean = false,
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
