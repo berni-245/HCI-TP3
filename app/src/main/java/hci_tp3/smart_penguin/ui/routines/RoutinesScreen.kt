0a6f876e3776af6c0492e69ccacfa34acd94e64c
tobitba <tpugliano@itba.edu.ar>
2024-06-19 20:36:21 -0300

    Chore: Add initial layout to RoutinesScreen

--- app/src/main/java/hci_tp3/smart_penguin/ui/routines/RoutinesScreen.kt
+++ app/src/main/java/hci_tp3/smart_penguin/ui/routines/RoutinesScreen.kt
@@ -1,24 +1,39 @@
 package hci_tp3.smart_penguin.ui.routines
 
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import hci_tp3.smart_penguin.ui.getViewModelFactory
+import androidx.compose.foundation.lazy.items
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun RoutinesScreen(
     viewModel: RoutinesViewModel = viewModel(factory = getViewModelFactory()),
 ) {
     val uiState by viewModel.uiState.collectAsState()
-
-    Text("Hello Routines")
+    val routineList = uiState.routines
+    if(routineList.isEmpty()){
+        NoRoutinesBox()
+    }
+    else{
+        LazyColumn (
+            verticalArrangement =  Arrangement.spacedBy(8.dp),
+            //contentPadding = PaddingValuesz,
+        ) {
+            items(items = routineList){ item ->
+                RoutineCard(routineName = item.name, routineDescription = "Routina" + item.name + " desc not implemented")
+            }
+        }
+    }
 }
 
-@Preview(showSystemUi = true, showBackground = true,
+@Preview( showBackground = true,
     device = "id:pixel_3"
 )
 @Composable
