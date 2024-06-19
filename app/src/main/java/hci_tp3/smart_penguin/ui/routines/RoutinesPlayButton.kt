14e6551028417b1ba0da4f1b6f06980cf99b1d9f
tobitba <tpugliano@itba.edu.ar>
2024-06-19 12:59:14 -0300

    Feat: Create RoutinePlayButton for Routine card.

---
+++ app/src/main/java/hci_tp3/smart_penguin/ui/routines/RoutinesPlayButton.kt
@@ -0,0 +1,52 @@
+package hci_tp3.smart_penguin.ui.routines
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun RoutinePlayButton(
+    enabled: Boolean = true,
+    onClick: () -> Unit
+) {
+    Button(
+        onClick = onClick,
+        colors = ButtonColors(
+            contentColor = Color.White,
+            disabledContentColor = Color.LightGray ,
+            containerColor = Color.Green,
+            disabledContainerColor = Color.DarkGray
+        ),
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Icon(
+            imageVector = Icons.Filled.PlayArrow,
+            contentDescription = "Play",
+            tint = Color.White,
+            modifier = Modifier.size(15.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true, device = "id:pixel")
+@Composable
+fun RoutinePlayButtonPreview(){
+    RoutinePlayButton {
+
+    }
+}
+
