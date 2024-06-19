6f3c8d1c7501268799926a05583de73055d535e5
tobitba <tpugliano@itba.edu.ar>
2024-06-19 20:37:06 -0300

    Fix: Add empty routinesList screen

---
+++ app/src/main/java/hci_tp3/smart_penguin/ui/routines/NoRoutinesBox.kt
@@ -0,0 +1,29 @@
+package hci_tp3.smart_penguin.ui.routines
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+@Preview(showBackground = true)
+@Composable
+fun NoRoutinesBox() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "No has agregado rutinas")
+        }
+    }
+}
