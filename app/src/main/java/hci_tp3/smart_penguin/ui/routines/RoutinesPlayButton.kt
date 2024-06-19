f901b9a155abb63ed0935cee8a0023c3e5fc66da
tobitba <tpugliano@itba.edu.ar>
2024-06-19 13:01:09 -0300

    Chore: Remove unused packages and variables

--- app/src/main/java/hci_tp3/smart_penguin/ui/routines/RoutinesPlayButton.kt
+++ app/src/main/java/hci_tp3/smart_penguin/ui/routines/RoutinesPlayButton.kt
@@ -1,26 +1,21 @@
 package hci_tp3.smart_penguin.ui.routines
 
-import androidx.annotation.StringRes
-import androidx.compose.foundation.layout.fillMaxWidth
+
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun RoutinePlayButton(
-    enabled: Boolean = true,
     onClick: () -> Unit
 ) {
     Button(
