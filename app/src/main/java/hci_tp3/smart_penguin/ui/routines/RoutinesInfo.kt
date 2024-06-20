43e2e094f960e9a482067b098b0686f3ffc96691
tobitba <tpugliano@itba.edu.ar>
2024-06-20 17:46:03 -0300

    Chore: Add Lamp to routines info

--- app/src/main/java/hci_tp3/smart_penguin/ui/routines/RoutinesInfo.kt
+++ app/src/main/java/hci_tp3/smart_penguin/ui/routines/RoutinesInfo.kt
@@ -1,22 +1,27 @@
 package hci_tp3.smart_penguin.ui.routines
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import hci_tp3.smart_penguin.model.Action
+import hci_tp3.smart_penguin.model.DeviceType
 import hci_tp3.smart_penguin.model.Lamp
 import hci_tp3.smart_penguin.model.Routine
 import hci_tp3.smart_penguin.model.state.Status
@@ -41,24 +46,8 @@ fun RoutinesInfo(
                     .height(130.dp)
                     .padding(16.dp)
             ){
-                Text(text = action.device.name,
-                    modifier = Modifier.padding(16.dp),
-                    style = MaterialTheme.typography.headlineSmall.copy(
-                        fontWeight = FontWeight.Bold
-                    )
-                    )
-                Row (
-                    modifier = Modifier.padding(start = 36.dp)
-                ) {
-                    Text(text = action.actionName)
-                    if(action.params.isNotEmpty()){
-                        Text(text = " : ")
-                        action.params.forEach { param ->
-                            Text(text = param)
-                        }
-                    }
-                }
 
+                HandleDeviceType(action = action)
             }
 
         }
@@ -66,6 +55,88 @@ fun RoutinesInfo(
 
    }
 }
+
+@Composable
+fun HandleDeviceType(action : Action){
+
+    when(action.device.type){
+        DeviceType.BLIND -> BlindHandler(action)
+        DeviceType.LAMP -> LampHandler(action)
+        DeviceType.AC -> AcHandler(action)
+        DeviceType.VACUUM -> VacuumHandler(action)
+    }
+
+
+}
+
+@Composable
+fun PrintActionName(name : String){
+    Text(text = name,
+        modifier = Modifier.padding(16.dp),
+        style = MaterialTheme.typography.headlineSmall.copy(
+            fontWeight = FontWeight.Bold
+        )
+    )
+}
+
+
+@Composable
+fun BlindHandler(action : Action){
+   /* TODO Icon(painter = , contentDescription = )*/
+    PrintActionName(action.actionName)
+}
+
+
+@Composable
+fun LampHandler(action : Action){
+    /* TODO Icon(painter = , contentDescription = )*/
+    PrintActionName(action.device.name)
+    Row (
+        modifier = Modifier.padding(start = 36.dp)
+    ) {
+        Text(text = action.actionName)
+
+        if(action.params.isNotEmpty()){
+            Text(text = " : ")
+            when(action.actionName){
+                Lamp.SET_COLOR_ACTION -> {
+                    val color =  android.graphics.Color.parseColor("#${action.params.first()}")
+                    Box(
+                        modifier = Modifier
+                            .width(20.dp)
+                            .height(20.dp)
+                            .background(color = Color(color)) // Specify the desired background color here
+                    ) {}
+                }
+                Lamp.SET_BRIGHTNESS_ACTION ->{
+                    Text(text = action.params.first())
+                }
+                else -> {
+
+                }
+            }
+
+        }
+    }
+}
+
+@Composable
+fun AcHandler(action : Action){
+    /* TODO Icon(painter = , contentDescription = )*/
+    PrintActionName(action.actionName)
+}
+
+@Composable
+fun VacuumHandler(action : Action){
+    /* TODO Icon(painter = , contentDescription = )*/
+    PrintActionName(action.actionName)
+}
+
+
+
+
+
+
 
 @Composable
 fun RoutineInfoScreen(
@@ -79,9 +150,13 @@ fun RoutineInfoScreen(
 @Composable
 fun RoutinesInfoPreview(){
     HCITP3Theme {
+
         var d1 = Lamp("12","Lamparita",status = Status.OFF, color = "FFFFFF", brightness = 10, room = null)
-        var a1 = Action(device = d1,actionName = "TurnOn",params = listOf("hola"),null)
-        var actions = listOf(a1)
+        var a1 = Action(device = d1,actionName = "setColor",params = listOf("3068FF"),null)
+        var a2 = Action(device = d1,actionName = "setBrightness",params = listOf("60"),null)
+        var a3 = Action(device = d1,actionName = "turnOn",params = emptyList(),null)
+        var a4 = Action(device = d1,actionName = "turnOff",params = emptyList(),null)
+        var actions = listOf(a1,a2,a3,a4)
         var routine = Routine(id= "1234", name= "Mi Rutina Genial",actions,null)
         RoutinesInfo(routine = routine)
     }
