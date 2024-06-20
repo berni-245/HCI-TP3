655e65cac5eda2959eaf4317bac6ddcfb7cba75e
Franco Morroni <fmorroni99@gmail.com>
2024-06-20 06:04:21 -0300

    chore: remove unused room screen

--- app/src/main/java/hci_tp3/smart_penguin/ui/navigation/AppDestinations.kt
+++ app/src/main/java/hci_tp3/smart_penguin/ui/navigation/AppDestinations.kt
@@ -9,7 +9,6 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import hci_tp3.smart_penguin.R
 
 enum class AppDestinations(@StringRes val title: Int, val icon: ImageVector, val route: String) {
-    ROOMS(R.string.Rooms, Icons.Filled.Home, "rooms_screen"),
     DEVICES(R.string.Devices, Icons.Filled.CoffeeMaker, "devices_screen"),
     ROUTINES(R.string.Routines, Icons.Filled.Task, "routines_screen")
 }
--- app/src/main/java/hci_tp3/smart_penguin/ui/navigation/AppNavGraph.kt
+++ app/src/main/java/hci_tp3/smart_penguin/ui/navigation/AppNavGraph.kt
@@ -5,18 +5,14 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import hci_tp3.smart_penguin.ui.devices.DevicesScreen
-import hci_tp3.smart_penguin.ui.rooms.RoomsScreen
 import hci_tp3.smart_penguin.ui.routines.RoutinesScreen
 
 @Composable
 fun AppNavGraph(navController: NavHostController) {
     NavHost(
         navController = navController,
-        startDestination = AppDestinations.ROOMS.route
+        startDestination = AppDestinations.DEVICES.route
     ) {
-        composable(AppDestinations.ROOMS.route) {
-            RoomsScreen()
-        }
         composable(AppDestinations.DEVICES.route) {
             DevicesScreen()
         }
