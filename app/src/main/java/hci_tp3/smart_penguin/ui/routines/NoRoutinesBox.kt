1433aee478e8651c61e02c97ed9aedf98fc1a5c9
tobitba <tpugliano@itba.edu.ar>
2024-06-20 00:20:01 -0300

    Chore: Change tipography on NoRoutinesBox.

--- app/src/main/java/hci_tp3/smart_penguin/ui/routines/NoRoutinesBox.kt
+++ app/src/main/java/hci_tp3/smart_penguin/ui/routines/NoRoutinesBox.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -23,7 +24,10 @@ fun NoRoutinesBox() {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "No has agregado rutinas")
+            Text(
+                text = "No has agregado rutinas",
+                style = MaterialTheme.typography.headlineMedium
+            )
         }
     }
 }
