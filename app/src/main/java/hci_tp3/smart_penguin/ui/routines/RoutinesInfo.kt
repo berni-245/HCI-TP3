d040a83b6d23172afc2800eef76ddd1b2fcef78f
tobitba <tpugliano@itba.edu.ar>
2024-06-20 00:30:34 -0300

    Feat: Add RoutinesInfo.kt

---
+++ app/src/main/java/hci_tp3/smart_penguin/ui/routines/RoutinesInfo.kt
@@ -0,0 +1,12 @@
+package hci_tp3.smart_penguin.ui.routines
+
+//import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun RoutinesInfo(){
+   /* Scaffold {
+
+    }*/
+}
