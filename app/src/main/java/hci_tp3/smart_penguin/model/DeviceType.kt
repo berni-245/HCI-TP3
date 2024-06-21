ab0f787db69d851bb324eae4088da7ee379fe47f
tobitba <tpugliano@itba.edu.ar>
2024-06-20 22:52:48 -0300

    Fix: remove null on icon of DeviceType

--- app/src/main/java/hci_tp3/smart_penguin/model/DeviceType.kt
+++ app/src/main/java/hci_tp3/smart_penguin/model/DeviceType.kt
@@ -4,7 +4,7 @@ import androidx.annotation.DrawableRes
 import hci_tp3.smart_penguin.R
 import hci_tp3.smart_penguin.model.state.LocaleEnum
 
-enum class DeviceType(@StringRes override val resourceId: Int, @DrawableRes val icon: Int?): LocaleEnum {
+enum class DeviceType(@StringRes override val resourceId: Int, @DrawableRes val icon: Int): LocaleEnum {
     LAMP(R.string.lamps, R.drawable.ic_lamp),
     BLIND(R.string.blinds, R.drawable.ic_blind),
     AC(R.string.acs, R.drawable.ic_ac),
