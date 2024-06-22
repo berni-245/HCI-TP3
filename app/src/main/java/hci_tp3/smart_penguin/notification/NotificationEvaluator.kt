b45d9d8c1d9721e8b0b296bc09ea1ac9e56b2fe4
tobitba <tpugliano@itba.edu.ar>
2024-06-22 13:16:37 -0300

    Feat: add low battery on vacuum notification

--- app/src/main/java/hci_tp3/smart_penguin/notification/NotificationEvaluator.kt
+++ app/src/main/java/hci_tp3/smart_penguin/notification/NotificationEvaluator.kt
@@ -7,6 +7,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.compose.ui.res.stringResource
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import hci_tp3.smart_penguin.R
 import hci_tp3.smart_penguin.remote.DeviceRemoteDataSource
 import hci_tp3.smart_penguin.remote.api.RetrofitClient
@@ -20,9 +21,9 @@ import kotlinx.coroutines.launch
 class NotificationEvaluator() : BroadcastReceiver() {
 
     @OptIn(DelicateCoroutinesApi::class)
-    override fun onReceive(context: Context?, intent: Intent?) {
-
-        GlobalScope.launch(Dispatchers.IO) {
+    override fun onReceive(context: Context, intent: Intent) {
+        notification(context,intent)
+       /* GlobalScope.launch(Dispatchers.IO) {
             val deviceRemoteDataSource = DeviceRemoteDataSource(RetrofitClient.deviceService)
             val lowBatteryVacuum: ArrayList<String>? =
                 intent?.getStringArrayListExtra("low_battery_vacuum")
@@ -47,7 +48,7 @@ class NotificationEvaluator() : BroadcastReceiver() {
                 }
             }
 
-        }
+        }*/
     }
 
     private fun showNotificationLowBattery(
@@ -66,6 +67,31 @@ class NotificationEvaluator() : BroadcastReceiver() {
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .setAutoCancel(true)
                 .setContentIntent(pendingIntent)
+        }
+    }
+
+    private fun notification(
+        context: Context,
+        intent: Intent?
+    ) {
+
+        var builder = context.let {
+            val pendingIntent: PendingIntent =
+                PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+            NotificationCompat.Builder(it, "vacuumChannel")
+                .setSmallIcon(R.drawable.ic_vacuum)
+                .setContentTitle("Low Battery")
+                .setContentText("aaaaaa robocop se queda sin bateriaaaa ayudaa")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+        }
+        try {
+            val notificationManager = NotificationManagerCompat.from(context)
+            if(notificationManager.areNotificationsEnabled())
+                notificationManager.notify(1,builder.build())
+        }catch (_: SecurityException){
+
         }
     }
 
