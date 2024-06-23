2fc494c3f2e5618ee8d5c4c88be70141d26d40c8
tobitba <tpugliano@itba.edu.ar>
2024-06-23 20:58:57 -0300

    Feat: Add no routines found images

--- app/src/main/java/hci_tp3/smart_penguin/ui/routines/NoRoutinesBox.kt
+++ app/src/main/java/hci_tp3/smart_penguin/ui/routines/NoRoutinesBox.kt
@@ -1,18 +1,24 @@
 package hci_tp3.smart_penguin.ui.routines
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-@Preview(showBackground = true)
+import hci_tp3.smart_penguin.R
+
+@Preview(showBackground = true, locale = "es")
 @Composable
 fun NoRoutinesBox() {
     Box(
@@ -24,9 +30,11 @@ fun NoRoutinesBox() {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                text = "No has agregado rutinas",
-                style = MaterialTheme.typography.headlineMedium
+            Image(
+                painter = painterResource(id = R.drawable.no_routines_bg),
+                contentDescription = null,
+                contentScale = ContentScale.Fit,
+                modifier = Modifier.size(500.dp),
             )
         }
     }
