a78c617def81da7850ff8f385ad41fca6f1c528a
tobitba <tpugliano@itba.edu.ar>
2024-06-23 20:03:15 -0300

    Feat: add notificationsEnabled boolean to have in app setting

--- app/src/main/java/hci_tp3/smart_penguin/notification/NotificationGlobalVars.kt
+++ app/src/main/java/hci_tp3/smart_penguin/notification/NotificationGlobalVars.kt
@@ -2,3 +2,4 @@ package hci_tp3.smart_penguin.notification
 
 val lowBatteryVacuum :  MutableList<String> = mutableListOf()
 val chargedVacuum:  MutableList<String> = mutableListOf()
+var notifictionsEnabled = true
