d735741e289730cfa15739b5f17d7dc50d2f869a
tobitba <tpugliano@itba.edu.ar>
2024-06-23 19:50:40 -0300

    Fix: routine color

--- app/src/main/java/hci_tp3/smart_penguin/ui/routines/RoutineCard.kt
+++ app/src/main/java/hci_tp3/smart_penguin/ui/routines/RoutineCard.kt
@@ -1,25 +1,31 @@
 package hci_tp3.smart_penguin.ui.routines
 
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import hci_tp3.smart_penguin.model.Routine
 import hci_tp3.smart_penguin.ui.getViewModelFactory
 import hci_tp3.smart_penguin.ui.navigation.AppDestinations
+import hci_tp3.smart_penguin.ui.theme.HCITP3Theme
 
 
 @Composable
@@ -40,8 +46,9 @@ fun RoutineCard(
        }
     }
 
-
-    ElevatedCard(
+    HCITP3Theme {
+        ElevatedCard(
+            colors = CardColors(containerColor = colorScheme.primaryContainer, contentColor = Color.Black, disabledContentColor = Color.White, disabledContainerColor = Color.White ),
         onClick = {
             viewModel.setCurrentRoutine(routine)
             onNavigateDestination(AppDestinations.CURRENT_ROUTINE.route)
@@ -77,5 +84,23 @@ fun RoutineCard(
             routine.id?.let { RoutinePlayButton (it,viewModel) }
         }
 
-    }
+    }}
+}
+
+
+@Preview
+@Composable
+fun Prueb(){
+
+
+    ElevatedCard(
+        colors = CardColors(containerColor = colorScheme.primaryContainer, contentColor = Color.White, disabledContentColor = Color.White, disabledContainerColor = Color.White )
+        ,elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(130.dp)
+            .padding(16.dp)
+    ){}
 }
